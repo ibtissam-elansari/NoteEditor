38046a593671118a42497d7e7e5f888fc6ef2e29
Ibtissam EL <ibtissamel742@gmail.com>
2025-09-24 21:45:51 +0100

    implemented the custom fropdown for both creating and editing notes

--- src/components/CustomDropdown.jsx
+++ src/components/CustomDropdown.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 
 export default function CustomDropdown({selectedTags}) {
   const [isOpen, setIsOpen] = useState(false);
-  const [selected, setSelected] = useState(selectedTags && [...selectedTags]);
+  const [selected, setSelected] = useState(selectedTags ? [...selectedTags] : []);
 
   const Notes = useSelector(state => state.notes.notes);
 
@@ -45,7 +45,7 @@ export default function CustomDropdown({selectedTags}) {
         <ChevronDown className="mt-2 mr-1" size={18} />
       {isOpen && (
         <ul className="absolute left-0 mt-12 w-full bg-white border-0 rounded-lg shadow-lg z-10">
-          {Tags.filter(tag => !selected.includes(tag)).map((tag, i) => (
+          {Tags.filter(tag => !selected?.includes(tag)).map((tag, i) => (
             <li
               key={i}
               className={`px-4 py-2 cursor-pointer hover:bg-gray-100`}
