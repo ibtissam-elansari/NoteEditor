3086df5981aef26c1fb2ba678f23d8e64311ba45
Ibtissam EL <ibtissamel742@gmail.com>
2025-09-24 22:03:55 +0100

    improvements on CreateEditNote

--- src/pages/CreateEditNote.jsx
+++ src/pages/CreateEditNote.jsx
@@ -10,26 +10,38 @@ function CreateEditNote() {
   
   const note = id ? Notes.find(note => note.id === id) : null;
 
-  const allTags = [...new Set(Notes.flatMap(note => note.tags))];
-
   const [title, setTitle] = useState(id ? note.title : "");
-  const [tag, setTag] = useState(id ? note.tags : []);
+  const [body, setBody] = useState(id ? note.body : "");
 
-  return (
-    <div className="max-w-4xl mx-auto p-4">
-      <h2>Edit Note</h2>
-      <form action="" className='flex flex-col space-y-4 mt-5 w-full sm:flex-row space-x-5 justify-center'>
-        <div className='w-full md:w-1/2'>
-          <label htmlFor="">Title</label>
-          <input type="text" className='w-full my-1 border-1 border-gray-300 rounded-xs p-[0.45rem]' value={title} onChange={(e) => setTitle(e.target.value)}/>
+return (
+  <div className="max-w-4xl mx-auto p-4">
+    <h2>Edit Note</h2>
+    <form className="flex flex-col mt-5 w-full space-y-4">
+      <div className="flex flex-col md:flex-row md:space-x-5 w-full space-y-4 md:space-y-0">
+        <div className="w-full md:w-1/2">
+          <label>Title</label>
+          <input
+            type="text"
+            className="w-full my-1 border-1 border-gray-300 rounded-xs p-[0.45rem]"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-        <div className='w-full md:w-1/2'>
-          <label htmlFor="">Tags</label>
-          <CustomDropdown selectedTags={id && note.tags}/>
+        <div className="w-full md:w-1/2">
+          <label>Tags</label>
+          <CustomDropdown selectedTags={id && note.tags} />
         </div>
-      </form>
-    </div>
-  )
+      </div>
+      <div className="w-full">
+        <label>Body</label>
+        <textarea rows={25} value={body} onChange={(e) => setBody(e.target.value)}
+          className="w-full my-2 border-1 border-gray-300 rounded-xs p-[0.45rem] resize-none"
+        />
+      </div>
+    </form>
+  </div>
+);
+
 }
 
 export default CreateEditNote;
